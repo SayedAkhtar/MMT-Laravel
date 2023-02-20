02bf49dd144dd5ef344e7337c04e90f4f44d05d0
Sayed Akhtar <sayed.akhtar@techconfer.in>
2023-02-20 21:40:32 +0530

    Added_banners

--- app/Http/Controllers/API/Client/HomeController.php
+++ app/Http/Controllers/API/Client/HomeController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\Client\DoctorCollection;
 use App\Models\Doctor;
 use App\Models\Hospital;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class HomeController extends AppBaseController
 {
@@ -18,11 +19,17 @@ class HomeController extends AppBaseController
 
     public function modules()
     {
+        $files = File::allFiles(public_path('/img/banners/'));
+        $livePath = [];
+        foreach ($files as $img){
+            $livePath[] = asset('/img/banners/'.$img->getBasename());
+        }
         $hospitals = Hospital::query()->orderByDesc('created_at')->limit(10)->get();
         $doctors = Doctor::with('user')->orderByDesc('created_at')->limit(10)->get();
         $doctors = DoctorCollection::make($doctors)->resolve();
         $data['hospitals'] = $hospitals;
         $data['doctors'] = $doctors['data'];
+        $data['banners'] = $livePath;
         return $this->successResponse($data);
     }
 }
