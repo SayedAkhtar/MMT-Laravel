29ff20fb0bad4260bbc471991c6099252830afb8
Sayed Akhtar(Mac M1) <sayed182@gmail.com>
2023-05-30 05:37:13 +0530

    Added starter data

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Accreditation;
+use App\Models\Designation;
 use App\Models\Specialization;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class SpecializationSeeder extends Seeder
@@ -15,6 +18,35 @@ class SpecializationSeeder extends Seeder
      */
     public function run()
     {
-        Specialization::factory()->count(15)->create();
+        $d = [];
+        if (($open = fopen(__DIR__ . '/data.csv', "r")) !== FALSE) {
+
+            while (($data = fgetcsv($open, 1000, ",")) !== FALSE) {
+                $d[] = $data;
+            }
+
+            fclose($open);
+        }
+        foreach ($d as $k => $line) {
+            if ($k == 0) {
+                continue;
+            }
+            Specialization::create(['name' => $line[0], 'added_by' => User::where('email', 'admin@gmail.com')->first()->id]);
+        }
+        foreach ($d as $k => $line) {
+            if ($k == 0) {
+                continue;
+            }
+            Accreditation::create(['name' => $line[2], 'added_by' => User::where('email', 'admin@gmail.com')->first()->id]);
+        }
+
+        foreach ($d as $k => $line) {
+            if ($k == 0) {
+                continue;
+            }
+            Designation::create(['name' => $line[3], 'added_by' => User::where('email', 'admin@gmail.com')->first()->id]);
+        }
+
+//        Specialization::factory()->count(15)->create();
     }
 }
