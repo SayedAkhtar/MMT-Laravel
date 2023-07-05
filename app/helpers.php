8cb51a9a8b45f04c1bbeb38bbb228a2b6705b17a
Sayed Akhtar(Mac M1) <sayed182@gmail.com>
2023-07-05 13:01:33 +0530

    Updated otp sending

--- app/helpers.php
+++ app/helpers.php
@@ -91,7 +91,6 @@ if (!function_exists('sendMsg91OTP')) {
         $mobile = trim($mobile, '+');
         $client = new \GuzzleHttp\Client();
         $url = "https://control.msg91.com/api/v5/otp?template_id=648ae47fd6fc057a7101bb53&mobile=$mobile&otp=$code";
-        dump($url);
         try {
             $response = $client->request('POST', $url, [
                 'headers' => [
