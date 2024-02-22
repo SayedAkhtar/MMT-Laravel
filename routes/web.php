7c79aebc5fd85a09e9eab4bf7ce05751894590fd
Sayed Akhtar(Mac M1) <sayed182@gmail.com>
2024-02-23 02:16:27 +0530

    creds removed

--- routes/web.php
+++ routes/web.php
@@ -19,27 +19,27 @@ use Kreait\Firebase\Messaging\ApiClient;
 |
 */
 
-Route::get('/creds', function () {
-    $credentialsPath = '/Users/apple/Desktop/Freelance/MMT/Backend/mmt-firebase-admin-v3.json';
-    $d = json_decode(file_get_contents($credentialsPath), true);
-    // Create ServiceAccountCredentials object
-    $credentials = new ServiceAccountCredentials(['https://www.googleapis.com/auth/firebase.messaging'], $d);
+// Route::get('/creds', function () {
+//     $credentialsPath = '/Users/apple/Desktop/Freelance/MMT/Backend/mmt-firebase-admin-v3.json';
+//     $d = json_decode(file_get_contents($credentialsPath), true);
+//     // Create ServiceAccountCredentials object
+//     $credentials = new ServiceAccountCredentials(['https://www.googleapis.com/auth/firebase.messaging'], $d);
 
-    // Obtain the access token
-    try {
-        $scope = 'https://www.googleapis.com/auth/firebase.messaging';
-// $credentials = ApplicationDefaultCredentials::getCredentials($scope);
-$tempauxarrayresult = $credentials->fetchAuthToken();
-        // $accessToken = new AccessToken($credentials);
-        // $token = $accessToken->fetchAuthToken();
-        // $accessTokenString = $token['access_token'];
-        dd($tempauxarrayresult);
-        // Use $accessTokenString as needed
-    } catch (\Exception $e) {
-        // Handle exception
-        dd($e->getMessage());
-    }
-});
+//     // Obtain the access token
+//     try {
+//         $scope = 'https://www.googleapis.com/auth/firebase.messaging';
+// // $credentials = ApplicationDefaultCredentials::getCredentials($scope);
+// $tempauxarrayresult = $credentials->fetchAuthToken();
+//         // $accessToken = new AccessToken($credentials);
+//         // $token = $accessToken->fetchAuthToken();
+//         // $accessTokenString = $token['access_token'];
+//         dd($tempauxarrayresult);
+//         // Use $accessTokenString as needed
+//     } catch (\Exception $e) {
+//         // Handle exception
+//         dd($e->getMessage());
+//     }
+// });
 
 //Route::group(['middleware' => ['auth']], function () {
 Route::get('/agora-chat', [App\Http\Controllers\AgoraVideoController::class, 'index']);
